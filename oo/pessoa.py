6923dfe229eb7398d3777e469824151d9e2d06de
Emanuel <emanuelfilipess@gmail.com>
2021-09-06 22:07:10 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar()) #se tiver mais um argumento dentro do parenteses da erro pq o p ja foi passado no começo
-    print(p.nome)
-    p.nome = 'Emanuel'
-    print(p.nome)
-    print(p.idade)
+    luciano = Pessoa(nome='Luciano')
+    emanuel = Pessoa(luciano, nome='Emanuel')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar()) #se tiver mais um argumento dentro do parenteses da erro pq o p ja foi passado no começo
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
